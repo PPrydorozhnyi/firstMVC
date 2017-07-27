b8f0fc32551b32b36b751592e409551905811302
Peter Pridorozhny <Peter35128@gmail.com>
2017-07-27 21:18:42 +0300

    Added hardcode dao

--- src/main/java/system/controller/UserController.java
+++ src/main/java/system/controller/UserController.java
@@ -1,7 +1,13 @@
 package system.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by drake on 27/07/17.
  */
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
 }
---
+++ src/main/java/system/dao/UserDao.java
@@ -0,0 +1,24 @@
+package system.dao;
+
+import org.springframework.stereotype.Repository;
+import system.model.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by drake on 27/07/17.
+ */
+
+@Repository
+public class UserDao {
+
+
+    private List<User> users = Arrays.asList(new User("admin", "admin"),
+            new User("test", "test"));
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+}
