6067b3b3cb3dd1d3c7b6612c65fb22a0fd6a2d32
Peter Pridorozhny <Peter35128@gmail.com>
2017-07-28 10:09:51 +0300

    Added ModelAndView to validate users

--- src/main/java/system/controller/UserController.java
+++ src/main/java/system/controller/UserController.java
@@ -2,9 +2,11 @@ package system.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 import system.model.User;
 import system.service.UserService;
 
@@ -26,5 +28,25 @@ public class UserController {
     public @ResponseBody
     List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
+    public ModelAndView validateUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        // unite UI view and user model
+        modelAndView.addObject("userFromServer", new User());
+        // page to view
+        modelAndView.setViewName("users_check_page");
+        return modelAndView;
+    }
+
+    //from UI
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public @ResponseBody String checkUser(@ModelAttribute("userFromServer") User user) {
+
+        if ("admin".equals(user.getPassword()) && "admin".equals(user.getName()))
+            return "valid";
+        return "invalid";
+
     }
 }
